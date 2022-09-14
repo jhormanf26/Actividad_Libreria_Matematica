0575a447474720245cc629a9a01d19fd5b81f2a5
jhormanf26 <82169503+jhormanf26@users.noreply.github.com>
2022-09-13 20:46:56 -0500

    Math.random()

--- main.js
+++ main.js
@@ -1,4 +1,2 @@
-let numero = Number(prompt(`1 escribe un numero base`,2));
-let numero1 = Number(prompt(`2 escribe un numero al que quiere elevarlo`,4));
-console.log(`el resultado de  elevar ${numero} entre ${numero1} es: `);
-console.log(Math.pow(numero,numero1));
+
+console.log(Math.floor(Math.random()*6+1));
