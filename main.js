ff40fd235993b8a0de06623813813cf0926372fb
jhormanf26 <82169503+jhormanf26@users.noreply.github.com>
2022-09-13 20:27:26 -0500

    Math.max()

--- main.js
+++ main.js
@@ -1,2 +1,5 @@
-let numero = Number(prompt(`escribe un numero`,2.4));
-console.log(Math.trunc(numero));
+let numero = Number(prompt(`1 escribe un numero`,2.4));
+let numero1 = Number(prompt(`2 escribe un numero`,2.4));
+let numero2 = Number(prompt(`3 escribe un numero`,2.4));
+console.log(`el numero mayor entre ${numero}, ${numero1}, ${numero2} es: `);
+console.log(Math.max(numero,numero1,numero2));
