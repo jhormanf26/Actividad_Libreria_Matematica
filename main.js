937d75e6cd16db2f813299ee497a4ade9941f4ce
jhormanf26 <82169503+jhormanf26@users.noreply.github.com>
2022-09-13 20:32:50 -0500

    Math.min()

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 let numero = Number(prompt(`1 escribe un numero`,2.4));
 let numero1 = Number(prompt(`2 escribe un numero`,2.4));
 let numero2 = Number(prompt(`3 escribe un numero`,2.4));
-console.log(`el numero mayor entre ${numero}, ${numero1}, ${numero2} es: `);
-console.log(Math.max(numero,numero1,numero2));
+console.log(`el numero menor entre ${numero}, ${numero1}, ${numero2} es: `);
+console.log(Math.min(numero,numero1,numero2));
